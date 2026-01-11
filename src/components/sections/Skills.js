90a938477598f0b1a400a6e8ee5c125cd3ec08c4
apaseay <aypase@gmail.com>
2026-01-11 22:35:25 +0000

    add Linux OS skill and fix skills layout

--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -3,33 +3,21 @@ import Resume from "../../resume.json";
 import Skill from "../elements/Skill"
 
 
-function buildSkillsRow(skills, start, end) {
-  const batch = skills.slice(start, end);
-  let skillsComponents = [];
-  batch.forEach((value, indx) => {
-    skillsComponents.push(
-      <div key={indx} className="column is-3">
-        <Skill
-          text={value.name}
-          icon={value.x_icon}
-          description={value.x_description}
-        />
-      </div>
-    )
-  });
-  return skillsComponents;
-}
-
 function Skills() {
   return (
     <section className="section" id="skills">
       <div className="container">
         <h1 className="title">Skills</h1>
-        <div className="columns">
-          {buildSkillsRow(Resume.skills, 0, 4)}
-        </div>
-        <div className="columns">
-          {buildSkillsRow(Resume.skills, 4, 8)}
+        <div className="columns is-multiline">
+          {Resume.skills.map((skill, indx) => (
+            <div key={indx} className="column is-3">
+              <Skill
+                text={skill.name}
+                icon={skill.x_icon}
+                description={skill.x_description}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </section>
